1996b5e8df182dda5a8366a734a4ef4c657814a3
Tim Sawtell <timothy.sawtell@gmail.com>
2019-06-23 10:54:04 +1000

    removed tests

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SwiftUI_RowDucksTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftUI_RowDucksTests.allTests()
-XCTMain(tests)
--- Tests/SwiftUI-RowDucksTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(SwiftUI_RowDucksTests.allTests),
-    ]
-}
-#endif
